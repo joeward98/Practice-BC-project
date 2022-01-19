3bd1dbeb0f748f1c805e8a4ecbe9eea45068b6c0
joeward98 <joe@space48.com>
2022-01-19 11:03:34 +0000

    function deployed on GCP and resppnding to cart deletion webhook

--- src/cart-deletion-response-fn.ts
+++ src/cart-deletion-response-fn.ts
@@ -1,10 +1,18 @@
 import { HttpFunction } from "@google-cloud/functions-framework";
+import { GcpConfig } from "@space48/cloud-seed";
 
 const cartDeletionResponse: HttpFunction = async (req, res) => {
   if (req.method.toUpperCase() === "POST") {
-    res.send(`Cart ${req.body.data.id} deleted`);
+    console.log(`Cart ${req.body.data.id} deleted`);
+    return res.sendStatus(200);
   }
-  res.status(400).send("wrong");
+  return res.sendStatus(400);
 };
 
 export default cartDeletionResponse;
+
+export const runtimeConfig: GcpConfig = {
+  cloud: "gcp",
+  type: "http",
+  public: true,
+};
